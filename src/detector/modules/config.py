792bba353eee6fbe051e159aaa37087353922bd7
Farbod Parkhooi <farbodparkhooi@duck.com>
2026-08-06 18:22:49 +0330

    Update config.py

--- src/detector/modules/config.py
+++ src/detector/modules/config.py
@@ -2,5 +2,6 @@ from dataclasses import dataclass, field
 
 @dataclass(frozen=True)
 class DetectorConfigs():
+    # Backbone
     backbone_arch:str = "resnext101_32x8d"
     backbone_pretrained:bool = True
