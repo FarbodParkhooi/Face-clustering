c940e9d93592744a7cc7d3692f055831f0e06154
Farbod Parkhooi <farbodparkhooi@duck.com>
2026-08-07 18:48:35 +0330

    Created RPN Head + Anchor Generator
    + Created RPN Head model
    + Created anchor generator

---
+++ src/detector/models/rpn.py
@@ -0,0 +1,59 @@
+from modules.config import DetectorConfigs
+from torch import nn
+import torchvision
+import torch
+
+cfg = DetectorConfigs()
+
+class RPN_Head(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.cnv1 = nn.Conv2d(in_channels=cfg.fpn_channels, out_channels=cfg.rpn_conv_channels, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+
+        self.cnv2 = nn.Conv2d(in_channels=cfg.rpn_conv_channels, out_channels=cfg.num_anchors, kernel_size=1, stride=1, padding=0) 
+        self.cnv3 = nn.Conv2d(in_channels=cfg.rpn_conv_channels, out_channels=cfg.num_anchors*4, kernel_size=1, stride=1, padding=0) # (dx, dy, dw, dh per anchor)
+
+    def forward(self, x):
+        x = self.cnv1(x)
+        x = self.relu(x)
+
+        objectness_logits = self.cnv2(x)
+        regression_deltas = self.cnv3(x)
+
+        return (objectness_logits, regression_deltas)
+
+def generate_anchors(level_name:str, featurs:tuple, stride:int):
+    y_centers, x_centers, anchors = [], [], []
+    base_scale = cfg.base_scales[level_name]
+    # Calculating centers
+    for i in range(featurs[0]):  y_centers.append((i + 0.5)*stride)
+    for j in range(featurs[1]):  x_centers.append((j + 0.5)*stride)
+    # Calculating width/height for each anchor
+    for y_center in y_centers:
+        for x_center in x_centers:
+            for scale in cfg.scales:
+                for aspect_ratio in cfg.aspect_ratios:
+                    anchor_width  = base_scale * scale * (aspect_ratio**0.5)
+                    anchor_height = base_scale * scale / (aspect_ratio**0.5)
+                    # Calculating the points
+                    x1 = x_center - anchor_width  / 2
+                    y1 = y_center - anchor_height / 2
+                    x2 = x_center + anchor_width  / 2
+                    y2 = y_center + anchor_height / 2
+                    # Adding output positions
+                    anchors.append((x1, y1, x2, y2))
+    # Returning all anchors
+    return torch.tensor(anchors, dtype=torch.float32)
+
+def id_anchors(pyramid_features):
+    all_anchors = []
+    level_ids = []
+    for level_name, feat_map in pyramid_features.items():
+        _, _, H, W = feat_map.shape
+        stride = cfg.pyramid_strides[level_name]
+        anchors = generate_anchors(level_name, (H, W), stride) 
+        all_anchors.append(anchors)
+        level_ids.append(torch.full((anchors.shape[0],), cfg.level_to_index[level_name], dtype=torch.long))
+    return torch.cat(all_anchors, dim=0), torch.cat(level_ids, dim=0)
--- src/detector/modules/config.py
+++ src/detector/modules/config.py
@@ -9,3 +9,38 @@ class DetectorConfigs():
     # PANet
     fpn_channels:int = 512
     use_p6:bool = True 
+
+    # Anchor
+    scales:list[int] = field(default_factory=lambda: [0.5, 0.75, 1.0, 1.5, 2.0])
+    aspect_ratios:list[int] = field(default_factory=lambda: [0.5, 1.0, 1.5, 2.0])
+    num_anchors:int = len(scales) * len(aspect_ratios)  # 5 * 4 = 20
+    rpn_conv_channels:int = 128
+    base_scales:dict = {
+        "P2" :  4*4,  # Stride * CONSTANT
+        "P3" :  8*4,  # Stride * CONSTANT
+        "P4" : 16*8,  # Stride * CONSTANT
+        "P5" : 32*8,  # Stride * CONSTANT
+        "P6" : 64*4   # Stride * CONSTANT
+    }
+    pyramid_strides:dict = {
+        "P2" :  4,
+        "P3" :  8,
+        "P4" : 16,
+        "P5" : 32,
+        "P6" : 64
+    }
+    level_to_index:dict = {
+        "P2" : 0,
+        "P3" : 1,
+        "P4" : 2,
+        "P5" : 3,
+        "P6" : 4
+    }
+    anchors_per_image:int = 1024
+    rpn_positive_fraction:int = 0.5
+    rpn_pre_nms_top_n:int = 10000
+    rpn_post_nms_top_n:int = 512
+
+    # CIoU
+    rpn_positive_iou_thresh = 0.65
+    rpn_negative_iou_thresh = 0.45
