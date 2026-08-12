37c67620e76112ffe40865aaf3664ec431fc5b6a
Farbod Parkhooi <farbodparkhooi@duck.com>
2026-08-12 16:28:18 +0330

    Update roi_head.py
    + Created ciou_loss function

--- src/detector/models/roi_head.py
+++ src/detector/models/roi_head.py
@@ -1,3 +1,4 @@
+from models.rpn import delta_decoder
 from torch import nn
 import torchvision
 import torch 
@@ -14,3 +15,44 @@ def assign_levels(proposals, canonical_scale, canonical_level, min_level, max_le
     level = level.clamp(min=min_level, max=max_level)
 
     return level.long() 
+
+def ciou_loss(pred_boxes, gt_boxes):
+    # center‑size conversion for predicted boxes
+    pred_cx = (pred_boxes[:, 0] + pred_boxes[:, 2]) / 2.0
+    pred_cy = (pred_boxes[:, 1] + pred_boxes[:, 3]) / 2.0
+    pred_w  = pred_boxes[:, 2] - pred_boxes[:, 0]
+    pred_h  = pred_boxes[:, 3] - pred_boxes[:, 1]
+    # center‑size conversion for ground-truth boxes
+    gt_cx = (gt_boxes[:, 0] + gt_boxes[:, 2]) / 2.0
+    gt_cy = (gt_boxes[:, 1] + gt_boxes[:, 3]) / 2.0
+    gt_w  = gt_boxes[:, 2] - gt_boxes[:, 0]
+    gt_h  = gt_boxes[:, 3] - gt_boxes[:, 1] 
+    center_dist = (pred_cx - gt_cx)**2 + (pred_cy - gt_cy)**2
+    # Finding coordinates of intersection rectangle 
+    inter_x1 = torch.max(pred_boxes[:, 0], gt_boxes[:, 0])
+    inter_y1 = torch.max(pred_boxes[:, 1], gt_boxes[:, 1])
+    inter_x2 = torch.min(pred_boxes[:, 2], gt_boxes[:, 2])
+    inter_y2 = torch.min(pred_boxes[:, 3], gt_boxes[:, 3])
+    inter_area = (inter_x2 - inter_x1).clamp(min=0) * (inter_y2 - inter_y1).clamp(min=0)
+    # Computing area of predicted and ground truth 
+    pred_area = pred_w * pred_h
+    gt_area   = gt_w * gt_h
+    union_area = pred_area + gt_area - inter_area
+    iou = inter_area / (union_area + 1e-7)
+    # Coordinates of the smallest rectangle that completely covers both boxes
+    encl_x1 = torch.min(pred_boxes[:, 0], gt_boxes[:, 0])
+    encl_y1 = torch.min(pred_boxes[:, 1], gt_boxes[:, 1])
+    encl_x2 = torch.max(pred_boxes[:, 2], gt_boxes[:, 2])
+    encl_y2 = torch.max(pred_boxes[:, 3], gt_boxes[:, 3])
+    encl_diag = (encl_x2 - encl_x1)**2 + (encl_y2 - encl_y1)**2 + 1e-7
+    # Converting aspect ration to angle 
+    pred_angle = torch.atan(pred_w / (pred_h + 1e-7))
+    gt_angle   = torch.atan(gt_w   / (gt_h + 1e-7))
+    v = (4 / (torch.pi**2)) * ((gt_angle - pred_angle)**2)
+    # Computing a balancing weight (alpha) that depends on IoU and v
+    with torch.no_grad():
+        alpha = v / (1 - iou + v + 1e-7)
+    # Complete CIoU score
+    ciou = iou - (center_dist / encl_diag) - (alpha * v)
+    loss = 1 - ciou
+    return loss.mean()
